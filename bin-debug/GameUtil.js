e072301db85403d53bd48830e55061f7b00a2fcb
 Du Shengzhe <dusz7@dusz7Mac.local>
2017-03-23 11:31:14 +0800

    改善了一下碰撞检测

--- bin-debug/GameUtil.js
+++ bin-debug/GameUtil.js
@@ -16,13 +16,36 @@ var GameUtil = (function () {
     };
     /**基于矩形的碰撞检测*/
     GameUtil.hitTest = function (obj1, obj2) {
-        var rect1 = obj1.getBounds();
+        //var rect1:egret.Rectangle = obj1.getBounds();
         var rect2 = obj2.getBounds();
-        rect1.x = obj1.x;
-        rect1.y = obj1.y + 10;
+        //rect1.x = obj1.x;
+        //rect1.y = obj1.y;
         rect2.x = obj2.x;
         rect2.y = obj2.y;
-        return rect1.intersects(rect2);
+        //return rect1.intersects(rect2);
+        if (obj1.hitTestPoint((rect2.left + rect2.width / 2), (rect2.height / 2 + rect2.top), true)) {
+            console.log("hit it");
+            console.log(rect2.x + "," + rect2.y);
+            console.log((rect2.x + rect2.width / 2) + "," + (rect2.height / 2 + rect2.top));
+            console.log(obj1.y);
+            return true;
+        }
+        /*
+        if(obj1.hitTestPoint(rect2.bottomRight.x,rect2.bottomRight.y,true)){
+            //console.log("bottomRight");
+            //return true;
+        }else if(obj1.hitTestPoint(rect2.right,rect2.y,true)){
+            //console.log("upRight");
+            //return true;
+        }else if(obj1.hitTestPoint(rect2.x,rect2.bottom,true)){
+            //console.log("bottomLeft");
+            //return true;
+        }else if(obj1.hitTestPoint(rect2.x,rect2.y,true)){
+            //console.log("upLeft");
+            //return true;
+        }
+        */
+        return false;
     };
     return GameUtil;
 }());
